f40aba002e00bd6c8fcb7e9210a2755f801bc91a
HCioffi42 <122919391+HCioffi42@users.noreply.github.com>
2023-06-22 21:04:41 -0300

    Delete Projeto Academia/SpringBlogApp directory

--- Projeto Academia/SpringBlogApp/src/main/java/com/example/springbootblogapplication/repositories/AuthorityRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.springbootblogapplication.repositories;
-
-import com.example.springbootblogapplication.models.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, String> {}

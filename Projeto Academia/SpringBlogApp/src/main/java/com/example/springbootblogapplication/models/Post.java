c5faf8a16fd0a7d149f92bfd569dcd303e372471
HCioffi42 <henrique.cioffi@atos.net>
2023-06-28 08:15:01 -0300

    Frontend implementations

---
+++ Projeto Academia/SpringBlogApp/src/main/java/com/example/springbootblogapplication/models/Post.java
@@ -0,0 +1,48 @@
+package com.example.springbootblogapplication.models;
+
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Post {
+
+    @Id
+    @SequenceGenerator(name = "post_sequence", sequenceName = "post_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "post_sequence")
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String body;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "account_id", referencedColumnName = "id", nullable = false)
+    private Account account;
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + "'" +
+                ", body='" + body + "'" +
+                ", createdAt='" + createdAt + "'" +
+                ", updatedAt='" + updatedAt + "'" +
+                "}";
+    }
+
+}
